295bb19e82d4d386f8f25543ae464ffd3218edd9
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-08-15 14:27:30 +0900

    front setting

---
+++ front/src/pages/InvestingDetail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InvestingDetail() {
+    return
+}
+
+export default InvestingDetail
---
+++ front/src/pages/InvestingList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function InvestingList() {
+    return (
+        <div> hi</div>
+    )
+}
+
+export default InvestingList

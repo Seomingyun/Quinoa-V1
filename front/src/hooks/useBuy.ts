b43bd3fe6ce1d64c22031672b19d70827d3a9eaf
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-08-31 04:36:06 +0900

    toast modal logic complete

--- front/src/hooks/useBuy.ts
+++ front/src/hooks/useBuy.ts
@@ -1,5 +1,5 @@
 import {ethers, BigNumber} from 'ethers';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { Router__factory, ERC20__factory, TestToken__factory } from 'contract';
 import { text } from 'stream/consumers';
 
@@ -7,25 +7,42 @@ export const useBuy = (amount:string, address:any, assetAddress:any, currentAddr
     const {ethereum} =window;
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = useMemo(()=>provider?.getSigner(), [provider]);
+    const [txStatus, setTxStatus] = useState<string>("default");
 
     const buy = useCallback(
         async(amount:string, currentAddress:any, address:any, assetAddress:string, ) => {
-            
+
             const routerAddress:string = process.env.REACT_APP_ROUTER_ADDRESS || ""
             const router = Router__factory.connect(routerAddress, signer);
             
             //**Testtoken minting just for testing **
-            // const testToken = TestToken__factory.connect(assetAddress, signer);
-            // const mintTx = await testToken.mint(currentAddress, ethers.utils.parseUnits("10000"));
-            // await mintTx.wait();
-            // console.log("minted!");
-            const asset = ERC20__factory.connect(assetAddress, signer);
-            await asset.approve(router.address, ethers.utils.parseUnits(amount));
-            const buyTx = await router['buy(address,uint256)'](address, ethers.utils.parseUnits(amount));
-            await buyTx.wait();
-            console.log("buy!");
+            const testToken = TestToken__factory.connect(assetAddress, signer);
+            try{
+                const mintTx = await testToken.mint(currentAddress, ethers.utils.parseUnits("1"));
+                console.log(mintTx);
+                setTxStatus(mintTx.blockHash === null ? "pending" : "error");
+                const receipt = await mintTx.wait();
+                console.log(receipt);
+                setTxStatus(!!receipt.blockHash ? "success" : "error" );
+                //setTxStatus("complete");
+            } catch(e) {
+                console.log(e);
+                setTxStatus("error");
+            }
 
         }, [amount, address, assetAddress, currentAddress]);
-  
-    return {buy}
+
+    useEffect(()=>{
+        if(txStatus === "error"){
+            setTimeout(()=>{
+                setTxStatus("default")
+               }, 8000)
+        }
+        else if (txStatus === "success"){
+            setTimeout(()=>{
+                setTxStatus("default")
+               }, 4000)
+        }
+    },[txStatus]);
+    return {buy, txStatus};
 };
--- front/src/pages/InvestingDetail.tsx
+++ front/src/pages/InvestingDetail.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./InvestingDetail.css";
 import { useBuy } from "../hooks/useBuy";
 import { Link , useLocation, useParams} from "react-router-dom";
-
-
+import {Toast} from "../components/Modals/Toast";
 
 interface RouteState {
   state: {
@@ -11,25 +10,80 @@ interface RouteState {
   };
 }
 
+interface toastProperties {
+  data: 
+  {
+    title : string,
+    description : string,
+    backgroundColor:string
+  }| undefined;
+  close: () => void;
+
+}
+
 function InvestingDetail({currentAccount}:any) {
   const {address} = useParams();
-  console.log(address);
-
   const {state} = useLocation() as RouteState;
 
   const [amount,setAmount] = useState("0");
-  const {buy} = useBuy(amount, currentAccount, address, state.assetAddress);
+  const {buy, txStatus} = useBuy(amount, currentAccount, address, state.assetAddress);
   const handleChange= (e:any) => {
     setAmount(e.target.value);
   }
 
+  const [toastList, setToastList] = useState<toastProperties['data']|undefined>();
+  let toastProperty:toastProperties['data'];
+  const showToast = (type:string) => {
+    switch(type) {
+      case 'default':
+        toastProperty = undefined;
+        break;
+      case 'pending':
+        toastProperty = {
+          title: 'Pending',
+          description: 'Please Wait',
+          backgroundColor: '#5bc0de'
+        }
+        break;
+      case 'error':
+        toastProperty={
+          title: 'Failed',
+          description: 'Fail to buy',
+          backgroundColor: '#f0ad4e'
+        }
+        break;
+      case 'success':
+        toastProperty={
+          title: 'Success',
+          description: 'This is a success toast component',
+          backgroundColor: '#5cb85c'
+        }
+        break;
+      default:
+        toastProperty = undefined;
+    }
+    setToastList(toastProperty);
+  }
+
+  console.log("TRACKING STATUS", txStatus);
+
+  const closeToast: toastProperties['close'] =()=>{
+    setToastList(undefined);
+  }
+
+  useEffect(()=> {
+    showToast(txStatus);
+  },[txStatus])
+
   return (
+  <div>
   <div className="myinvest_banner">
     <input type="text" name="buyAmount" placeholder="Enter amount to buy" value={amount.toString()} onChange={handleChange}></input>
     <button type="button" onClick={() => buy(amount, currentAccount, address, state.assetAddress)}>buy</button>
   </div>
+  <Toast data={toastList} close={closeToast}/>
+  </div>
   )
-
 }
 
 export default InvestingDetail;
